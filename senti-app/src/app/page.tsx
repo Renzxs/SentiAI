ae5be823655a3df0591882c8168a97c6d7245ff9
Renzxs <florencebatol85@gmail.com>
2025-08-19 23:44:23 +0800

    added: bg effect

--- senti-app/src/app/page.tsx
+++ senti-app/src/app/page.tsx
@@ -1,17 +1,37 @@
 import { Container, VStack, Text, Image, HStack } from "@chakra-ui/react";
 import SignInBtn from "./signin-btn";
 import SentiLogo from "../../public/senti-logo.png";
+import DarkVeil from "@/components/darkveil-bg";
 
 export default function Home() {
   return (
     <Container
-      maxW="md"
-      height="100vh"
+      maxW="full"
       display="flex"
       alignItems="center"
       justifyContent="center"
+      position="relative"
+      height="100vh"
     >
-      <VStack align="stretch" gap={4} textAlign="center">
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+      >
+        <DarkVeil
+          hueShift={53}
+          scanlineIntensity={0.1}
+          speed={1}
+          warpAmount={0.5}
+        />
+      </div>
+      <VStack align="stretch" gap={4} textAlign="center" zIndex={1}>
         <VStack gap={2} mb={4}>
           <HStack justifyContent="center" alignItems="center" gap={2}>
             <Image src={SentiLogo.src} alt="SentiAI" width={30} height={30} />
